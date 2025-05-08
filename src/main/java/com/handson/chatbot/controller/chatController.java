4e168e11222284940297cea460bb7ac385b3866d
Natan <155454141+NatanProtector@users.noreply.github.com>
2025-05-08 21:04:20 +0300

    updating chat route

--- src/main/java/com/handson/chatbot/controller/chatController.java
+++ src/main/java/com/handson/chatbot/controller/chatController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class chatController {
 
-    @GetMapping("/chat")
+    @GetMapping("")
     public String homePage() {
         return "chat"; // corresponds to home.html
     }
